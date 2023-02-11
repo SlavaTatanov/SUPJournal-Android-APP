eccdb8c9ec30fad6bccea7e606fc4f2b5a2123ae
SlavaTatanov <slavatatanov@gmail.com>
2023-02-11 17:07:08 +0500

    authModule

--- app/src/main/java/com/st/supjournal/AuthActivity.kt
+++ app/src/main/java/com/st/supjournal/AuthActivity.kt
@@ -1,15 +1,25 @@
 package com.st.supjournal
 
+import android.app.Application
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.st.supjournal.databinding.ActivityAuthBinding
-import com.st.supjournal.network.ApiViewModel
+import com.st.supjournal.network.ApiService
+import com.st.supjournal.network.AuthBody
+import com.st.supjournal.network.AuthResponse
+import kotlinx.coroutines.launch
+import java.net.ConnectException
 
 
 class AuthActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAuthBinding
+    private lateinit var authViewModel: AuthViewModel
 
     private lateinit var sharedPref: SharedPreferences
     private lateinit var jwToken: String
@@ -18,9 +28,12 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        authViewModel = ViewModelProvider(this)[AuthViewModel::class.java]
+        authViewModel.init(binding)
 
         /*
         Получаем доступ к внутреннему хранилищу и забираем из него JWT токен
+        если он есть
          */
         sharedPref = getSharedPreferences("App_Pref", MODE_PRIVATE)
         jwToken = sharedPref.getString("JWT", "").toString()
@@ -29,9 +42,66 @@ class AuthActivity : AppCompatActivity() {
         Обрабатываем нажатие клавиши войти, выполняет http запрос на сервер
          */
         binding.btn.setOnClickListener {
-            val login = binding.editLogin.text.toString()
-            val pass = binding.editPass.text.toString()
-            ApiViewModel().apiAuth(login, pass, binding)
+            authViewModel.apiAuth()
         }
+        binding.test.text = jwToken
+    }
+
+    private fun startMain(){
+        intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
+
+/**
+ * ViewModel для AuthActivity
+ */
+class AuthViewModel(application: Application): AndroidViewModel(application) {
+
+    lateinit var binding: ActivityAuthBinding
+
+    fun init (binding: ActivityAuthBinding) {
+       this.binding = binding
+    }
+
+    /**
+     * Запрос к базе данных (Передаем логин пароль)
+     */
+    fun apiAuth() {
+        if (inputCheck()) {
+            var res: AuthResponse
+            val login = binding.editLogin.text.toString()
+            val pass = binding.editPass.text.toString()
+            viewModelScope.launch {
+                try {
+                    res = ApiService.retrofitService.auth(AuthBody(login, pass)).body()!!
+                    if (res.msg == "ok") {
+                        binding.test.text = res.token
+                    } else {
+                        binding.test.text = res.msg
+                    }
+
+                } catch (e: ConnectException) {
+                    val err = getApplication<Application?>().getString(R.string.connection_error)
+                    binding.test.text = err
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Проверяем что поля login и password заполнены
+     */
+    private fun inputCheck (): Boolean{
+        var flag = true
+        for (it in arrayOf(binding.editLogin, binding.editPass)) {
+                if (it.text.isNullOrEmpty()) {
+                    it.error = getApplication<Application?>().getString(R.string.input_error)
+                    flag = false
+                }
+            }
+        return flag
+    }
+}
+
--- app/src/main/java/com/st/supjournal/network/ApiService.kt
+++ app/src/main/java/com/st/supjournal/network/ApiService.kt
@@ -1,15 +1,13 @@
 package com.st.supjournal.network
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+
 import com.google.gson.annotations.SerializedName
-import com.st.supjournal.databinding.ActivityAuthBinding
-import kotlinx.coroutines.launch
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
+
 
 private const val BASE_URL = "http://10.0.2.2:5000"
 
@@ -26,7 +24,11 @@ data class AuthBody(
 
 data class AuthResponse(
     @SerializedName("token")
-    var token: String?
+    var token: String?,
+    @SerializedName("status")
+    var status: String?,
+    @SerializedName("msg")
+    var msg: String?
 )
 
 
@@ -38,19 +40,5 @@ interface ApiServiceService {
 object ApiService {
     val retrofitService: ApiServiceService by lazy {
         retrofit.create(ApiServiceService::class.java)
-    }
-}
-
-/**
- * Класс предоставляет доступ к API сервера, и реализует функции http запросов
- */
-class ApiViewModel: ViewModel() {
-
-    fun apiAuth(login: String, pass_: String, binding: ActivityAuthBinding) {
-        val res = AuthResponse("1")
-        viewModelScope.launch {
-            res.token = ApiService.retrofitService.auth(AuthBody(login, pass_)).body()?.token
-            binding.test.text = res.token
-        }
     }
 }
